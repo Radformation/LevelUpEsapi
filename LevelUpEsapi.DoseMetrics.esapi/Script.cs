7ee1a25ab20e4a8dbb6cc0426c5bda7bb077420b
Carlos Anderson <canderson@radformation.com>
2025-05-31 13:39:01 -0400

    Add initial solution and projects

---
+++ LevelUpEsapi.DoseMetrics.esapi/Script.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using VMS.TPS.Common.Model.API;
+
+namespace LevelUpEsapi.DoseMetrics.esapi
+{
+    public class Script
+    {
+        public void Execute(ScriptContext context)
+        {
+            MessageBox.Show(context.Patient.Id);
+        }
+    }
+}
---
+++ LevelUpEsapi.FakeEclipse/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using LevelUpEsapi.DoseMetrics.esapi;
+using VMS.TPS.Common.Model.API;
+using Application = System.Windows.Application;
+
+namespace LevelUpEsapi.FakeEclipse
+{
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var scriptContext = LoadScriptContext();
+            var script = new Script();
+            script.Execute(scriptContext);
+
+            // Needed because there's no main window, which would normally
+            // shutdown the app when the window is closed.
+            Shutdown();
+        }
+
+        private ScriptContext LoadScriptContext()
+        {
+            return new ScriptContext() { Patient = new Patient() { Id = "Test" } };
+        }
+    }
+}
