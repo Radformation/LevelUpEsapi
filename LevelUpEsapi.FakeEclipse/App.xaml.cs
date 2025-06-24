a3258089add60a23ef13c592db9ae011aaa35005
Carlos Anderson <canderson@radformation.com>
2025-06-24 17:35:07 -0400

    Clean up and add comments

--- LevelUpEsapi.FakeEclipse/App.xaml.cs
+++ LevelUpEsapi.FakeEclipse/App.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Media;
 using VMS.TPS;
 using VMS.TPS.Common.Model.API;
 using Application = System.Windows.Application;
@@ -10,43 +9,17 @@ namespace LevelUpEsapi.FakeEclipse
     {
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
-            var scriptContext = LoadScriptContext();
+            // Create a fake ScriptContext with hard-coded data.
+            ScriptContext scriptContext = FakeScriptContext.Create();
+
+            // Create and execute the Script object that Eclipse would also execute,
+            // but because we are using a fake ScriptContext, we can run it locally.
             var script = new Script();
             script.Execute(scriptContext);
 
             // Needed because there's no main window, which would normally
             // shutdown the app when the window is closed.
             Shutdown();
-        }
-
-        private ScriptContext LoadScriptContext()
-        {
-            return new ScriptContext
-            {
-                Patient = new Patient
-                {
-                    Id = "Test"
-                },
-                StructureSet = new StructureSet()
-                {
-                    Structures = new []
-                    {
-                        new Structure
-                        {
-                            Id = "PTV",
-                            Color = Colors.Red,
-                            Volume = 11.87934
-                        },
-                        new Structure
-                        {
-                            Id = "Liver",
-                            Color = Colors.Blue,
-                            IsEmpty = true,
-                            Volume = 132.26453
-                        }
-                    }
-                }
-            };
         }
     }
 }
---
+++ LevelUpEsapi.FakeEclipse/FakeScriptContext.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Media;
+using VMS.TPS.Common.Model.API;
+
+namespace LevelUpEsapi.FakeEclipse
+{
+    public static class FakeScriptContext
+    {
+        /// <summary>
+        /// Creates a fake ScriptContext (from LevelUpEsapi.FakeEsapi project) with hard-coded data.
+        /// </summary>
+        /// <returns>A ScriptContext object with hard-coded data.</returns>
+        public static ScriptContext Create()
+        {
+            return new ScriptContext
+            {
+                Patient = new Patient
+                {
+                    Id = "Test"
+                },
+                StructureSet = new StructureSet()
+                {
+                    Structures = new []
+                    {
+                        new Structure
+                        {
+                            Id = "PTV",
+                            Color = Colors.Red,
+                            Volume = 11.87934
+                        },
+                        new Structure
+                        {
+                            Id = "Liver",
+                            Color = Colors.Blue,
+                            IsEmpty = true,
+                            Volume = 132.26453
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
